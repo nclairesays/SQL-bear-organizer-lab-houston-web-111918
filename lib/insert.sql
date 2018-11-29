928f3f1662e7cfe8ac006ad9e9aad9498be069fc
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-28 23:08:12 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name) 
+    VALUES 
+        ("Mr. Chocolate"), 
+        ("Rowdy"),
+        ("Tabitha"),
+        ("4"),
+        ("5"),
+        ("6"),
+        ("7"),
+        (null);
